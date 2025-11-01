24ac6a2afcf92c2afe9d0f01f033f84d0d60987c
aartibisht123 <aartbisht@gmail.com>
2025-11-01 17:05:24 +0530

    server/mongodb.js

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,11 +1,26 @@
+// import mongoose from "mongoose";
+
+// // connect to the mongodb database
+
+// const connectDB = async () =>{
+//     mongoose.connection.on('connected', ()=> console.log('Database connected'))
+
+//     await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+// }
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 
-// connect to the mongodb database
-
-const connectDB = async () =>{
-    mongoose.connection.on('connected', ()=> console.log('Database connected'))
-
-    await mongoose.connect(`${process.env.MONGODB_URL}/lms`)
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "lms",
+    });
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+  }
+};
 
 export default connectDB;

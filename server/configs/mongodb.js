82311e6b7c077f0864c74d233c86a7f26971ba4f
aartibisht123 <aartbisht@gmail.com>
2025-11-02 13:19:57 +0530

    server/mongodb.js -> server/configs/mongodb.js

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 // connect to the mongodb database
 
 const connectDB = async () =>{
+  mongoose.connection.on('connected', ()=> console.log('database connected'))
+       await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 }
 
   try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+   
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
--- server/server.js
+++ server/server.js
@@ -2,34 +2,31 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import connectDB from './mongodb.js'
+import connectDB from './configs/mongodb.js'
 import { clerkwebhooks } from './controllers/webhooks.js'
 
 
 // Initialize express 
-
 const app = express()
 app.use(express.json());
-//connect to database
- connectDB()
-// MiddleWares
 
+//connect to database
+await connectDB()
+
+// MiddleWares
 app.use(cors())
 
 // Routes
 app.get('/', (req, res)=> res.send("API WORKING"))
 
-
 app.post('/clerk', express.json(), clerkwebhooks)
-
-// app.post("/api/webhooks/clerk", clerkwebhooks);
-// app.post('/api/webhooks/clerk', express.json(), clerkwebhooks)
 
 
 // Prevent favicon.ico 500 error
 app.get('/favicon.ico', (req, res) => res.status(204).end());
-// Port
 
+
+// Port
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, ()=>{
